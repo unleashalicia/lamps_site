434005f865727624483f89c0ebd22eeeeb090f26
unleashalicia <alicia.adams.evans@gmail.com>
2018-03-23 16:00:38 -0700

    Added separate header classes

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from '../assets/images/lamps_transparent_logo.png';
 
 
-const Header = () => {
-    const headerStyle = {
-        backgroundColor: "#000000",
-        color: "white", 
-        position: "fixed",
-        height: "20vh",
-        width: "100vw",
-        zIndex: 5
-    };
+class Header extends Component {
+    constructor(props){
+        super(props);
 
-    const imgStyle = {
-        width: "20vw",
-        position: "absolute",
-        top: "50%",
-        left: "1%",
-        transform: "translateY(-50%)"
-    };
+        this.headerStyle = {
+            backgroundColor: "#000000",
+            color: "white",
+            position: "fixed",
+            height: "20vh",
+            width: "100vw",
+            zIndex: 5,
+            transition: "1s"
+        };
 
-    return (
-        <div style={headerStyle}>
-            <img style={imgStyle} src={logo} alt="LAMPS logo"/>
-        </div>
-    );
+        this.smallHeaderStyle = {
+            backgroundColor: "#000000",
+            color: "white",
+            position: "fixed",
+            height: "10vh",
+            width: "100vw",
+            zIndex: 5
+        };
+
+        this.imgStyle = {
+            height: "90%",
+            position: "absolute",
+            top: "50%",
+            left: "1%",
+            transform: "translateY(-50%)"
+        };
+    }
+
+    render(){
+        return (
+            <div style={this.headerStyle}>
+                <img style={this.imgStyle} src={logo} alt="LAMPS logo"/>
+            </div>
+        );
+    }
 };
             
 export default Header;
