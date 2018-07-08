9582e95fabdae14c0914b8623a45dc0657a8b49a
unleashalicia <alicia.adams.evans@gmail.com>
2018-07-07 21:11:39 -0700

    Style adjustments

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,22 +1,20 @@
 import React, {Component} from 'react';
-import content from '../helpers/content'
+import content from '../helpers/content';
+import logo from '../assets/images/lamps_transparent_logo.png';
 import '../assets/css/hero.css';
 
 class Hero extends Component {
     constructor(props){
         super(props);
 
-        this.imageStyle = {
-            backgroundImage: `url(${this.props.image})`
-        };
 
     }
 
     render(){
         const { header, subheader } = content.home.hero
         return (
-            <div className="hero" style={this.imageStyle}>
-                <h1>{header}</h1>
+            <div className="hero">
+                <img className="logo" src={logo} alt="lamps logo"/>
             </div>
         );
     }
